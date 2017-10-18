4ee81c6d62831688224ae8c6a7588620a7884a47
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-10-18 08:55:28 -0700

    Various updates for dxlbootstrap 0.1.3 compatibility

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+
 from setuptools import setup
 import distutils.command.sdist
 
@@ -6,18 +8,21 @@ import setuptools.command.sdist
 # Patch setuptools' sdist behaviour with distutils' sdist behaviour
 setuptools.command.sdist.sdist.run = distutils.command.sdist.sdist.run
 
-VERSION = __import__('dxlvtapiclient').get_version()
+version_info = {}
+cwd=os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(cwd, "dxlvtapiclient", "_version.py")) as f:
+    exec(f.read(), version_info)
 
 dist = setup(
     # Package name:
     name="dxlvtapiclient",
 
     # Version number:
-    version=VERSION,
+    version=version_info["__version__"],
 
     # Requirements
     install_requires=[
-        "dxlbootstrap",
+        "dxlbootstrap>=0.1.3",
         "dxlclient"
     ],
 
@@ -33,7 +38,11 @@ dist = setup(
     # Packages
     packages=[
         "dxlvtapiclient",
-    ],
+        "dxlvtapiclient._config",
+        "dxlvtapiclient._config.sample"],
+
+    package_data={
+        "dxlvtapiclient._config.sample" : ['*']},
 
     # Details
     url="http://www.mcafee.com/",
